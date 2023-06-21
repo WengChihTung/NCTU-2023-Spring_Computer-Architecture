d47a298f6ddcfd05479dabf1d7a413c53258eadd
WengChihTung <eric40915@gmail.com>
2023-06-21 18:36:46 +0800

    Fix main.cu and cuda1.cu to process matrix in double pointer

--- cuda1.cu
+++ cuda1.cu
@@ -1,31 +1,44 @@
 #include "parameters.h"
 
 
-__global__ void cuda_kernel(int dB[][SIZE],int dA[][SIZE],IndexSave dInd[][SIZE])
+__global__ void cuda_kernel(int **dB, int **dA, IndexSave **dInd)
 {	
-	int i=0;
-	int TotalThread = blockDim.x*gridDim.x;
-	int stripe = SIZE * SIZE / TotalThread;
-	int head   = (blockIdx.x*blockDim.x + threadIdx.x)*stripe;
-	int LoopLim = head+stripe;
-	
-	for(i=head ; i<LoopLim ; i++ ){
-		for(int j=0; j<SIZE; j++){
-			dInd[i][j].blockInd_x = blockIdx.x;
-			dInd[i][j].threadInd_x = threadIdx.x;
-			dInd[i][j].head = head;
-			dInd[i][j].stripe = stripe;
-			dB[i][j] += dA[i][j];
-			dB[i][j] += dA[i][j];
-		}
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if(idx < SIZE * SIZE){
+		int m = idx / SIZE;
+		int n = idx % SIZE;
+	 	dInd[m][n].blockInd_x = blockIdx.x;
+	 	dInd[m][n].threadInd_x = threadIdx.x;
+	 	dInd[m][n].head = idx;
+	 	dInd[m][n].stripe = 1;
+	 	dB[m][n] += dA[m][n];
+	 	dB[m][n] += dA[m][n];
+
 	}
+	// int i = 0;
+	// int TotalThread = blockDim.x * gridDim.x;
+	// int      stripe = SIZE * SIZE / TotalThread;
+	// int        head = (blockIdx.x * blockDim.x + threadIdx.x) * stripe;
+	// int     LoopLim = head + stripe;
+	
+	// for(i = head; i < LoopLim; i++){
+	// 	int m = i / SIZE;
+	// 	int n = i % SIZE;
+	// 	dInd[m][n].blockInd_x = blockIdx.x;
+	// 	dInd[m][n].threadInd_x = threadIdx.x;
+	// 	dInd[m][n].head = head;
+	// 	dInd[m][n].stripe = stripe;
+	// 	dB[m][n] += dA[m][n];
+	// 	dB[m][n] += dA[m][n];
+	// }
 };
 
 
-float GPU_kernel(int B[][SIZE],int A[][SIZE],IndexSave indsave[][SIZE]){
+float GPU_kernel(int **B, int **A, IndexSave **indsave){
 
-	//int dA[SIZE][SIZE],dB[SIZE][SIZE];
-	//IndexSave dInd[SIZE][SIZE];
+	int** dA; 
+	int** dB;
+	IndexSave** dInd;
 
 	// Creat Timing Event
   	cudaEvent_t start, stop;
@@ -33,39 +46,57 @@ float GPU_kernel(int B[][SIZE],int A[][SIZE],IndexSave indsave[][SIZE]){
 	cudaEventCreate (&stop); 	
 
 	// Allocate Memory Space on Device
+	cudaMalloc((void**)&dA, sizeof(int*) * SIZE);
+	cudaMalloc((void**)&dB, sizeof(int*) * SIZE);
 
 	// Allocate Memory Space on Device (for observation)
-	//cudaMalloc((void**)&dInd,sizeof(IndexSave)*SIZE);
+	cudaMalloc((void**)&dInd, sizeof(IndexSave*) * SIZE);
+	
+	for(int i = 0; i < SIZE; i++){
+		cudaMalloc((void**)&dA[i], sizeof(int) * SIZE);
+		cudaMalloc((void**)&dB[i], sizeof(int) * SIZE);
+		cudaMalloc((void**)&dInd[i], sizeof(IndexSave) * SIZE);
+	}
 
 	// Copy Data to be Calculated
-	//cudaMemcpy(dA, A, sizeof(int)*SIZE*SIZE, cudaMemcpyHostToDevice);
-	//cudaMemcpy(dB, B, sizeof(int)*SIZE*SIZE, cudaMemcpyHostToDevice);
+	for(int i = 0; i < SIZE; i++){
+		cudaMemcpy(dA[i], A[i], sizeof(int)*SIZE, cudaMemcpyHostToDevice);
+		cudaMemcpy(dB[i], B[i], sizeof(int)*SIZE, cudaMemcpyHostToDevice);
+		// Copy Data (indsave array) to device
+		cudaMemcpy(dInd[i], indsave[i], sizeof(IndexSave)*SIZE, cudaMemcpyHostToDevice);
+	}
 
-	// Copy Data (indsave array) to device
-	//cudaMemcpy(dInd, indsave, sizeof(IndexSave)*SIZE*SIZE, cudaMemcpyHostToDevice);
 	
 	// Start Timer
 	cudaEventRecord(start, 0);
 
-	// Lunch Kernel
-	dim3 dimGrid(2);
-	dim3 dimBlock(4);
-	cuda_kernel<<<dimGrid,dimBlock>>>(B,A,indsave);
+	// Launch Kernel
+	dim3 dimGrid(SIZE);
+	dim3 dimBlock(SIZE);
+	cuda_kernel<<<dimGrid,dimBlock>>>(dB,dA,dInd);
+	//cudaDeviceSynchronize();
 
 	// Stop Timer
 	cudaEventRecord(stop, 0);
   	cudaEventSynchronize(stop); 
 
 	// Copy Output back
-	//cudaMemcpy(A, dA, sizeof(int)*SIZE*SIZE, cudaMemcpyDeviceToHost);
-	//cudaMemcpy(B, dB, sizeof(int)*SIZE*SIZE, cudaMemcpyDeviceToHost);
-	
-	//cudaMemcpy(indsave, dInd, sizeof(IndexSave)*SIZE*SIZE, cudaMemcpyDeviceToHost);
+	for(int i = 0; i < SIZE; i++){
+		cudaMemcpy(A[i], dA[i], sizeof(int)*SIZE, cudaMemcpyDeviceToHost);
+		cudaMemcpy(B[i], dB[i], sizeof(int)*SIZE, cudaMemcpyDeviceToHost);
+		
+		cudaMemcpy(indsave[i], dInd[i], sizeof(IndexSave)*SIZE, cudaMemcpyDeviceToHost);
+	}
 
 	// Release Memory Space on Device
-	//cudaFree(dA);
-	//cudaFree(dB);
-	//cudaFree(dInd);
+	for(int i = 0; i < SIZE; i++){
+		cudaFree(dA[i]);
+		cudaFree(dB[i]);
+		cudaFree(dInd[i]);
+	}
+	cudaFree(dA);
+	cudaFree(dB);
+	cudaFree(dInd);
 
 	// Calculate Elapsed Time
   	float elapsedTime; 
--- main.cu
+++ main.cu
@@ -2,9 +2,9 @@
 
 #include "parameters.h"
 
-extern float GPU_kernel(int B[][SIZE],int A[][SIZE],IndexSave indsave[][SIZE]);
+extern float GPU_kernel(int **B, int **A, IndexSave **indsave);
 
-void genNumbers(int A[][SIZE], int size){
+void genNumbers(int **A, int size){
 	for(int i = 0; i < size; i++){
 		for(int j = 0; j < size; j++){
 			A[i][j] = rand()%256;
@@ -12,18 +12,18 @@ void genNumbers(int A[][SIZE], int size){
 	}
 }
 
-void function_1(int C[][SIZE],int A[][SIZE]){
-	for(int i=0;i<SIZE;i++){
-		for(int j=0;j<SIZE;j++){
-			C[i][j]=A[i][j];
-			C[i][j]+=A[i][j];
+void function_1(int **C, int **A){
+	for(int i = 0; i < SIZE; i++){
+		for(int j = 0; j < SIZE; j++){
+			C[i][j] = A[i][j];
+			C[i][j] += A[i][j];
 		}
 	}
 }
 
-bool verify(int A[][SIZE],int B[][SIZE]){
+bool verify(int **A, int **B){
 
-	for(int i=0;i<SIZE;i++){
+	for(int i = 0; i < SIZE; i++){
 		for(int j = 0; j < SIZE; j++){
 			if(A[i][j]!=B[i][j]) return true;
 		}
@@ -31,12 +31,12 @@ bool verify(int A[][SIZE],int B[][SIZE]){
 	return false;
 }
 
-void printIndex(IndexSave indsave[][SIZE],int B[][SIZE],int C[][SIZE])
+void printIndex(IndexSave **indsave, int **B, int **C)
 {
-	for(int i=0;i<SIZE;i++){
+	for(int i = 0; i < SIZE; i++){
 		for(int j = 0; j < SIZE; j++){
-			printf("%d, %d : blockInd_x=%d,threadInd_x=%d,head=%d,stripe=%d",i,j,(indsave[i][j]).blockInd_x,(indsave[i][j]).threadInd_x,(indsave[i][j]).head,(indsave[i][j]).stripe);
-			printf(" || GPU result=%d,CPU result=%d\n",B[i][j],C[i][j]);
+			printf("%d, %d : blockInd_x=%d, threadInd_x=%d, head=%d, stripe=%d", i, j, (indsave[i][j]).blockInd_x, (indsave[i][j]).threadInd_x, (indsave[i][j]).head, (indsave[i][j]).stripe);
+			printf(" || GPU result=%d, CPU result=%d\n",B[i][j],C[i][j]);
 		}
 	}
 }
@@ -44,29 +44,38 @@ void printIndex(IndexSave indsave[][SIZE],int B[][SIZE],int C[][SIZE])
 int main()
 {
 	// random seed
-	int A[SIZE][SIZE];
+	int **A = new int* [SIZE];
 	// random number sequence computed by GPU
-	int B[SIZE][SIZE];
+	int **B = new int* [SIZE];
 	// random number sequence computed by CPU
-	int C[SIZE][SIZE];
+	int **C = new int* [SIZE];
 	// Indices saver (for checking correctness)
-	IndexSave indsave[SIZE][SIZE];
+	IndexSave **indsave = new IndexSave* [SIZE];
+
+	for(int i = 0; i < SIZE; i++){
+		A[i] = new int[SIZE];
+		B[i] = new int[SIZE];
+		C[i] = new int[SIZE];
+		indsave[i] = new IndexSave[SIZE];
+	}
 	
 	genNumbers(A,SIZE);
-	memset( B, 0, sizeof(int)*SIZE*SIZE );
+	for(int i = 0; i < SIZE; i++){
+		memset(B[i], 0, sizeof(int) * SIZE);
+	}
 
 	/* CPU side*/
 	function_1(C,A);
 
 	/* GPU side*/
-	float elapsedTime = GPU_kernel(B,A,indsave);
+	float elapsedTime = GPU_kernel(B, A, indsave);
 
 	/*Show threads execution info*/
-	printIndex(indsave,B,C);
+	printIndex(indsave, B, C);
 
 	printf("==============================================\n");
 	/* verify the result*/
-	if(verify(B,C)){printf("wrong answer\n");}
+	if(verify(B, C)){printf("wrong answer\n");}
 	printf("GPU time = %5.2f ms\n", elapsedTime);
 
 	/*Please press any key to exit the program*/
